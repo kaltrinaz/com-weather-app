8101aa36d12c5ea202dc0308d3968590f11e4bf1
kaltrinaz <58449801+kaltrinaz@users.noreply.github.com>
2024-11-08 22:10:37 +0100

    added more weather data

--- src/script.js
+++ src/script.js
@@ -3,9 +3,32 @@ function updateTemp(response) {
     let tempElement = document.querySelector("#weather-temperature");
     let temperature = Math.round(response.data.temperature.current); 
     tempElement.innerHTML = temperature;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
     let cityElement = document.querySelector("#search-city");
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
     cityElement.innerHTML = response.data.city;
+}
+
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes},`;
 }
 
 
@@ -26,3 +49,5 @@ function searchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("London");
