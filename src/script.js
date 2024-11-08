d5a840849010679ff1f4f7466854518f78b5c4d0
kaltrinaz <58449801+kaltrinaz@users.noreply.github.com>
2024-11-08 20:32:39 +0100

    Added the basic of js

---
+++ src/script.js
@@ -0,0 +1,12 @@
+
+function searchSubmit(event) {
+    event.preventDefault();
+
+let searchInput = document.querySelector("#search-input");
+let cityElement = document.querySelector("#search-city");
+cityElement.innerHTML = searchInput.value;
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
