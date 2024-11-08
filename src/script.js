b2579ea3252642766cac69f5cf4b1d759d7cecdb
kaltrinaz <58449801+kaltrinaz@users.noreply.github.com>
2024-11-08 21:12:19 +0100

    Added API

--- src/script.js
+++ src/script.js
@@ -1,10 +1,26 @@
 
+function updateTemp(response) {
+    let tempElement = document.querySelector("#weather-temperature");
+    let temperature = Math.round(response.data.temperature.current); 
+    tempElement.innerHTML = temperature;
+
+    let cityElement = document.querySelector("#search-city");
+    cityElement.innerHTML = response.data.city;
+}
+
+
+function searchCity(city) {
+    let apiKey = "52t2ff0fofee82bd34831c3b064a32e5";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(updateTemp);
+    
+}
+
 function searchSubmit(event) {
     event.preventDefault();
 
-let searchInput = document.querySelector("#search-input");
-let cityElement = document.querySelector("#search-city");
-cityElement.innerHTML = searchInput.value;
+    let searchInput = document.querySelector("#search-input");
+    searchCity(searchInput.value);
 }
 
 
